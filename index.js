7e1a3275319e353b5c80bb6fc05aee8d4dc55ee0
Kyle E. Mitchell <kyle@kemitchell.com>
2017-02-12 19:57:23 -0800

    Implement requester logic.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var Estimator = require('strata-estimator')
 var IBF = require('ibf')
+var concat = require('concat-stream')
 var flushWriteStream = require('flush-write-stream')
+var pump = require('pump')
 var pumpify = require('pumpify')
 var rpc = require('rpc-multistream')
 var through = require('through2')
@@ -12,6 +14,7 @@ var seeds = [0x0000, 0x9999, 0xFFFF]
 var estimatorCellCount = 80
 
 module.exports = {
+
   responder: function requester (createKeyStream) {
     return rpc({
       reconcile: rpc.syncStream(function () {
@@ -35,45 +38,96 @@ module.exports = {
               estimatorOptions(buffer)
             )
             var ourEstimator = new Estimator(xtend(estimatorOptions))
-            createKeyStream().pipe(flushWriteStream(
-              function (key, _, done) {
-                ourEstimator.insert(key)
-                done()
-              },
-              function (flush) {
-                var estimatedDifference = theirEstimator
-                  .decode(ourEstimator)
-                // "To use an IBF effectively, we must determine the
-                // approximate size of the set difference, d, since
-                // approximately 1.5d cells are required to successfully
-                // decode the IBF."
-                // --- Eppstein et al, section 3.2.
-                var cellCount = Math.ceil(1.5 * estimatedDifference)
-                var filter = new IBF(filterOptions(cellCount))
-                createKeyStream().pipe(flushWriteStream(
-                  function (key, _, done) {
-                    filter.insert(key)
-                    done()
-                  },
-                  function (done) {
-                    done()
-                    flush(null, Buffer.concat([
-                      varint.encode(cellCount),
-                      filter.arrayBuffer
-                    ]))
-                  }
-                ))
-              }
-            ))
+            pump(
+              createKeyStream(),
+              flushWriteStream(
+                function (key, _, done) {
+                  ourEstimator.insert(key)
+                  done()
+                },
+                function (flush) {
+                  var estimatedDifference = theirEstimator
+                    .decode(ourEstimator)
+                  // "To use an IBF effectively, we must determine
+                  // the approximate size of the set difference, d,
+                  // since approximately 1.5d cells are required to
+                  // successfully decode the IBF."
+                  // --- Eppstein et al, section 3.2.
+                  var cellCount = Math.ceil(1.5 * estimatedDifference)
+                  var filter = new IBF(filterOptions(cellCount))
+                  createKeyStream().pipe(flushWriteStream(
+                    function (key, _, done) {
+                      filter.insert(key)
+                      done()
+                    },
+                    function (done) {
+                      done()
+                      flush(null, Buffer.concat([
+                        varint.encode(cellCount),
+                        filter.arrayBuffer
+                      ]))
+                    }
+                  ))
+                }
+              )
+            )
             done()
           })
         )
       })
     })
+  },
+
+  requester: function (createKeyStream, onDecoded) {
+    var client = rpc()
+    client.on('methods', function (remote) {
+      pump(
+        remote.reconcile(),
+        concat({encoding: 'buffer'}, function (buffer) {
+          var cellCount = varint.decode(buffer)
+          var countBytes = varint.decode.bytes
+          var strata = new Uint8Array(buffer, countBytes)
+          var theirFilter = new IBF(filterOptions(cellCount, strata))
+          keyStreamToFilter(
+            createKeyStream, cellCount,
+            function (error, ourFilter) {
+              if (error) {
+                onDecoded(error)
+              } else {
+                theirFilter.subtract(ourFilter)
+                var result = theirFilter.decode()
+                if (result === false) {
+                  onDecoded(new Error('Could not decode IBF.'))
+                } else {
+                  onDecoded(null, result)
+                }
+              }
+            }
+          )
+        })
+      )
+    })
+    return client
   }
 }
 
-// TODO Client logic for decoding varint prefix and rehydrating IBF
+function keyStreamToFilter (createKeyStream, cellCount, callback) {
+  var filter = new IBF(filterOptions(cellCount))
+  pump(
+    createKeyStream(),
+    flushWriteStream(function (key, _, done) {
+      filter.insert(key)
+      done()
+    }),
+    function (error) {
+      if (error) {
+        callback(error)
+      } else {
+        callback(null, filter)
+      }
+    }
+  )
+}
 
 function estimatorOptions (strata) {
   return {
@@ -86,8 +140,8 @@ function estimatorOptions (strata) {
   }
 }
 
-function filterOptions (cellCount) {
-  return {
+function filterOptions (cellCount, arrayBuffer) {
+  var returned = {
     cellCount: cellCount,
     keyHashes: seeds.map(function (seed) {
       return function (id) {
@@ -101,6 +155,10 @@ function filterOptions (cellCount) {
     hashSumElements: 1,
     hashSumView: Uint32Array
   }
+  if (arrayBuffer) {
+    returned.arrayBuffer = arrayBuffer
+  }
+  return returned
 }
 
 function binaryXXH (idBuffer) {
