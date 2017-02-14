daa1ff63d1e21bd9941b54d120f52358a6fa6b87
Kyle E. Mitchell <kyle@kemitchell.com>
2017-02-13 17:29:14 -0800

    Implement directly over half-opening duplex streams.

--- index.js
+++ index.js
@@ -4,70 +4,78 @@ var concat = require('concat-stream')
 var flushWriteStream = require('flush-write-stream')
 var from = require('from2')
 var pump = require('pump')
-var pumpify = require('pumpify')
-var rpc = require('rpc-multistream')
 var through = require('through2')
 var varint = require('varint')
 var xxh = require('xxhashjs').h32
 
 module.exports = {
 
-  responder: function requester (streamKeys) {
-    return rpc({
-      reconcile: rpc.syncStream(function () {
-        return pumpify(
-          chunksToArrayOfNodeBuffers(),
-          through.obj(function (chunks, _, done) {
-            var self = this
-            var arrayBuffer = Buffer.concat(chunks).buffer
-            var theirEstimator = new Estimator(
-              estimatorOptions(arrayBuffer)
-            )
-            makeEstimator(streamKeys, function (error, ourEstimator) {
-              if (error) return done(error)
-              var estimatedDifference = Math.max(
-                1, theirEstimator.decode(ourEstimator)
-              )
-              // "To use an IBF effectively, we must determine
-              // the approximate size of the set difference, d,
-              // since approximately 1.5d cells are required to
-              // successfully decode the IBF."
-              // --- Eppstein et al, section 3.2.
-              var cellCount = Math.ceil(1.5 * estimatedDifference)
-              makeFilter(
-                streamKeys, cellCount,
-                function (error, filter) {
-                  if (error) return done(error)
-                  self.push(new Buffer(varint.encode(cellCount)))
-                  self.push(new Buffer(filter.arrayBuffer))
-                  done()
-                }
-              )
-            })
-          })
+  respond: function (createKeyStream, connection) {
+    connection
+      .pipe(chunksToArrayOfNodeBuffers())
+      .pipe(through({
+        writableObjectMode: true,
+        readableObjectMode: false
+      }, function (chunks, _, done) {
+        var self = this
+        var arrayBuffer = Buffer.concat(chunks).buffer
+        var theirEstimator = new Estimator(
+          estimatorOptions(arrayBuffer)
         )
-      })
-    })
+        makeEstimator(createKeyStream, function (error, ourEstimator) {
+          if (error) return done(error)
+          var estimatedDifference = Math.max(
+            1, theirEstimator.decode(ourEstimator)
+          )
+          // "To use an IBF effectively, we must determine
+          // the approximate size of the set difference, d,
+          // since approximately 1.5d cells are required to
+          // successfully decode the IBF."
+          // --- Eppstein et al, section 3.2.
+          var cellCount = Math.max(
+            Math.ceil(1.5 * estimatedDifference),
+            8
+          )
+          makeFilter(
+            createKeyStream, cellCount,
+            function (error, filter) {
+              if (error) return done(error)
+              self.push(new Buffer(varint.encode(cellCount)))
+              self.push(new Buffer(filter.arrayBuffer))
+              done()
+            }
+          )
+        })
+      }))
+      .pipe(through(function (chunk, enc, done) {
+        done(null, chunk, enc)
+      }))
+      .pipe(connection)
   },
 
-  requester: function (streamKeys, onDecoded) {
-    var client = rpc()
-    client.on('methods', function (remote) {
-      makeEstimator(streamKeys, function (error, estimator) {
-        if (error) return onDecoded(error)
+  request: function (createKeyStream, connection, callback) {
+    makeEstimator(createKeyStream, function (error, estimator) {
+      if (error) return callback(error)
+
+      streamEstimatorNodeBuffers()
+        .pipe(connection)
+        .pipe(filterProcessor())
+
+      function streamEstimatorNodeBuffers () {
         var arrayBuffers = estimator._strata.map(function (filter) {
           return filter.arrayBuffer
         })
-        var estimatorStream = from(function (size, next) {
+        return from(function (size, next) {
           if (arrayBuffers.length === 0) {
             next(null, null)
           } else {
             next(null, new Buffer(arrayBuffers.shift()))
           }
         })
-        var reconcile = remote.reconcile()
-        var asNodeBuffer = {encoding: 'buffer'}
-        var decodeFilter = concat(asNodeBuffer, function (nodeBuffer) {
+      }
+
+      function filterProcessor () {
+        return concat({encoding: 'buffer'}, function (nodeBuffer) {
           var cellCount = varint.decode(nodeBuffer)
           var headerBytes = varint.decode.bytes
           // TODO Avoid copying the whole buffer.
@@ -79,25 +87,21 @@ module.exports = {
             filterOptions(cellCount, bodyArrayBuffer)
           )
           makeFilter(
-            streamKeys, cellCount,
+            createKeyStream, cellCount,
             function (error, ourFilter) {
-              if (error) return onDecoded(error)
+              if (error) return callback(error)
               theirFilter.subtract(ourFilter)
               var result = theirFilter.decode()
               if (result === false) {
-                onDecoded(new Error('Could not decode IBF.'))
+                callback(new Error('Could not decode IBF.'))
               } else {
-                onDecoded(null, result)
+                callback(null, result)
               }
             }
           )
         })
-        estimatorStream
-          .pipe(reconcile)
-          .pipe(decodeFilter)
-      })
+      }
     })
-    return client
   }
 }
 
@@ -158,9 +162,7 @@ function estimatorOptions (arrayBuffer) {
   if (arrayBuffer) {
     var strata = []
     var totalLength = arrayBuffer.byteLength
-    console.log('%s is %j', 'totalLength', totalLength)
     var filterSize = totalLength / estimatorStrataCount
-    console.log('%s is %j', 'filterSize', filterSize)
     var offset
     for (offset = 0; offset < totalLength; offset += filterSize) {
       strata.push(new IBF(filterOptions(
--- test.js
+++ test.js
@@ -1,46 +1,53 @@
 var assert = require('assert')
+var net = require('net')
 var crypto = require('crypto')
 var from = require('from2')
 var reconcile = require('./')
-var through = require('through2')
 
-var responder = reconcile.responder(function () {
-  return streamOfKeys(['a', 'b', 'c', 'd', 'e'])
+var alphabet = []
+for (var i = 65; i <= 90; i++) {
+  alphabet.push(String.fromCharCode(i))
+}
+
+// Create a TCP server that responds to reconciliation requests.
+var server = net.createServer({allowHalfOpen: true}, function (socket) {
+  reconcile.respond(keys(alphabet.slice(0, 20)), socket)
 })
 
-var requester = reconcile.requester(
-  function () {
-    return streamOfKeys(['f', 'g'])
-  },
-  function (error, decoded) {
-    assert.ifError(error)
-    console.log(decoded)
+// Start the server on a random high port.
+server.listen(0, function () {
+  var port = this.address().port
+  // Connect to the server.
+  var options = {port: port, allowHalfOpen: true}
+  var socket = net.connect(options, function () {
+    // Issue a reconciliation request.
+    reconcile.request(
+      keys(alphabet.slice(10, 26)),
+      socket,
+      function (error, difference) {
+        assert.ifError(error)
+        console.log(difference)
+        server.close()
+      }
+    )
+  })
+})
+
+// Helper Method:  Turn an array into a stream of ArrayBuffer digests.
+function keys (array) {
+  return function () {
+    var keyBuffers = array.map(function (string) {
+      return crypto.createHash('sha256')
+        .update(string)
+        .digest()
+        .buffer
+    })
+    return from.obj(function (size, done) {
+      if (keyBuffers.length <= 0) {
+        done(null, null)
+      } else {
+        done(null, keyBuffers.shift())
+      }
+    })
   }
-)
-
-requester
-  .pipe(through(function (chunk, _, done) {
-    console.log(chunk.length)
-    if (chunk.length < 3000) {
-      // console.log(chunk.toString('utf8'))
-    }
-    done(null, chunk)
-  }))
-  .pipe(responder)
-  .pipe(requester)
-
-function streamOfKeys (array) {
-  var keyBuffers = array.map(function (string) {
-    return crypto.createHash('sha256')
-      .update(string)
-      .digest()
-      .buffer
-  })
-  return from.obj(function (size, done) {
-    if (keyBuffers.length <= 0) {
-      done(null, null)
-    } else {
-      done(null, keyBuffers.shift())
-    }
-  })
 }
